55ced49065eda2a25d96fdb220caa8b79d9fba81
George Dinwiddie <gdinwiddie@idiacomputing.com>
2015-12-31 00:32:13 -0500

    implement CachingHoroscopeProvider as facade in front of any HoroscopeProvider using any HoroscopeCache

---
+++ src/main/java/com/gdinwiddie/equinehoroscope/CachingHoroscopeProvider.java
@@ -0,0 +1,36 @@
+package com.gdinwiddie.equinehoroscope;
+
+public class CachingHoroscopeProvider implements HoroscopeProvider {
+    private HoroscopeProvider horoscopeProvider;
+    private HoroscopeCache horoscopeCache;
+
+    public CachingHoroscopeProvider(HoroscopeProvider horoscopeProvider,
+            HoroscopeCache horoscopeCache) {
+        this.horoscopeProvider = horoscopeProvider;
+        this.horoscopeCache = horoscopeCache;
+    }
+
+    @Override
+    public String horoscopeFor(String horsename, String effectiveDate) {
+        String horoscope = null;
+        try {
+            horoscope = horoscopeCache.horoscopeFor(horsename, effectiveDate);
+        } catch (Exception e) {
+            // TODO Log this
+            System.err.println(e);
+        }
+        if (null == horoscope) {
+            horoscope = horoscopeProvider
+                    .horoscopeFor(horsename, effectiveDate);
+            try {
+                horoscopeCache
+                        .putHoroscope(horsename, effectiveDate, horoscope);
+            } catch (Exception e) {
+                // TODO Log this
+                System.err.println(e);
+            }
+        }
+        return horoscope;
+    }
+
+}
---
+++ src/test/java/com/gdinwiddie/equinehoroscope/CachingHoroscopeProviderTest.java
@@ -0,0 +1,53 @@
+package com.gdinwiddie.equinehoroscope;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CachingHoroscopeProviderTest {
+    private FakeHoroscopeProvider provider;
+    private HoroscopeCache cache;
+
+    @Before
+    public void setup() {
+        provider = new FakeHoroscopeProvider();
+        cache = new InMemoryHoroscopeCache();
+    }
+
+    @Test
+    public void canReceiveHoroscopeNotInCache() {
+        provider.addHoroscope("this is the horoscope for horse on date");
+        HoroscopeProvider cachingProvider = new CachingHoroscopeProvider(
+                provider, cache);
+        assertThat(cachingProvider.horoscopeFor("horse", "date"),
+                is("this is the horoscope for horse on date"));
+    }
+
+    @Test
+    public void canReceiveCachedHoroscope() {
+        HoroscopeProvider cachingProvider = new CachingHoroscopeProvider(
+                provider, cache);
+        provider.addHoroscope("first horoscope");
+        provider.addHoroscope("second horoscope");
+        cachingProvider.horoscopeFor("horse", "date");
+        assertThat(cachingProvider.horoscopeFor("horse", "date"),
+                is("first horoscope"));
+    }
+
+    @Test
+    public void ensureWeGetANewHoroscopeFromProviderForDifferentHorse() {
+        String horoscope = "some canned horoscope";
+        String differentHoroscope = "A different canned horoscope.";
+        provider.addHoroscope(horoscope);
+        provider.addHoroscope(differentHoroscope);
+        HoroscopeProvider cachingProvider = new CachingHoroscopeProvider(
+                provider, cache);
+        assertEquals(horoscope,
+                cachingProvider.horoscopeFor("Doesn't Matter", "today"));
+        assertEquals(differentHoroscope,
+                cachingProvider.horoscopeFor("Different Horse", "today"));
+    }
+}
